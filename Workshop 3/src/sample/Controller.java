a027e8e8b5fd01e90d3a0a769ef72e9faaf101ac
stephensander1161 <stephen.sander1@gmail.com>
2020-04-15 17:42:06 -0600

    added a couple functions, not working yet

--- Workshop 3/src/sample/Controller.java
+++ Workshop 3/src/sample/Controller.java
@@ -1,15 +1,26 @@
 package sample;
 
 import java.net.URL;
+import java.sql.*;
 import java.util.ResourceBundle;
+import java.util.Vector;
+
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.ListView;
 import javafx.scene.input.MouseEvent;
+import javax.swing.ComboBoxModel;
+import javax.swing.JComboBox;
 
 public class Controller {
+
+    private Connection conn;
+    private Statement stmt;
+    private ResultSet rs;
+
 
     @FXML
     private ResourceBundle resources;
@@ -21,7 +32,7 @@ public class Controller {
     private ListView<?> lvTable;
 
     @FXML
-    private ComboBox<?> cmbTable;
+    private ComboBox<String> cmbTable;
 
     @FXML
     private Button btnEdit;
@@ -57,8 +68,54 @@ public class Controller {
 
     }
 
+
+
+
+
+    private void connectDB() {
+        // TODO Auto-generated method stub
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/travelexperts", "Sander", "P@ssw0rd");
+            stmt = conn.createStatement();
+        } catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+
+    private Vector getAgentIDs() {
+        // TODO Auto-generated method stub
+        Vector<String> agents = new Vector<String>();
+        try {
+            rs = stmt.executeQuery("select agentid from agents");
+            while (rs.next())
+            {
+                agents.add(rs.getString("agentid"));
+            }
+        } catch (SQLException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return agents;
+    }
     @FXML
     void initialize() {
+
+
+
+
+
+
+
+
+
+
+
+
         assert lvTable != null : "fx:id=\"lvTable\" was not injected: check your FXML file 'sample.fxml'.";
         assert cmbTable != null : "fx:id=\"cmbTable\" was not injected: check your FXML file 'sample.fxml'.";
         assert btnEdit != null : "fx:id=\"btnEdit\" was not injected: check your FXML file 'sample.fxml'.";
