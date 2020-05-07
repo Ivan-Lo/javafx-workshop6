836d9d3a7814934c6e383300d003186dc10a6995
stephensander1161 <stephen.sander1@gmail.com>
2020-05-06 19:42:21 -0600

    ?

--- Workshop8wtabs/app/src/main/java/com/example/workshop8wtabs/MainActivity.java
+++ Workshop8wtabs/app/src/main/java/com/example/workshop8wtabs/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity {
         protected String doInBackground(Void... voids) {
             StringBuffer buffer = new StringBuffer();
             try {
-                URL url = new URL("http://192.168.1.131:8080/lab8/rs/customer/getallcustomers");
+                URL url = new URL("http://192.168.0.18:8080/Lab8-1/rs/customer/getallcustomers");
                 BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()));
                 String json;
                 while ((json = br.readLine()) != null) {
